17fc0f7788963676c5058568bbcad708b8b0661b
Felipe Bento <felipe.16costa@gmail.com>
2025-12-31 01:22:44 -0300

    chore: add ngxs for global state

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { withNgxsStoragePlugin } from '@ngxs/storage-plugin';
+import { provideStore } from '@ngxs/store';
 import { routes } from './app.routes';
+import { SessionState } from './utility/session/session.state';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideStore(
+      [SessionState],
+      withNgxsStoragePlugin({
+        keys: '*',
+      }),
+    ),
+  ],
 };
